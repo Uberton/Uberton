8ecd90bb7b1a10d6a57a1f36effad98cd5aa6a93
Mc-zen <alphabetagamma11@hotmail.com>
2021-09-02 19:44:45 +0200

    work on installer

--- src/installer/cmake/Installer.cmake
+++ src/installer/cmake/Installer.cmake
@@ -26,6 +26,21 @@ endfunction()
 
 include(cmake/CMakeRC.cmake)
 
+
+function(uberton_add_cmrc_resource)
+	set(args NAME FILE WHENCE)
+    cmake_parse_arguments(PARSE_ARGV 0 ARG "${options}" "${args}" "${list_args}")
+    message(STATUS "adding file to resources from folder \"${ARG_FOLDER}\": ${ARG_FILE}")
+	
+    if(DEFINED ARG_WHENCE)
+        set(whence_arg WHENCE "${ARG_WHENCE}")
+    endif()
+
+    cmrc_add_resource_library(rc_${ARG_NAME} ALIAS ${ARG_NAME} NAMESPACE ${ARG_NAME} ${ARG_FILE})
+    target_link_libraries(${target} PUBLIC ${ARG_NAME})
+	set_target_properties(rc_${ARG_NAME} PROPERTIES ${UBERTON_INSTALLER_RESOURCE_FOLDER})
+endfunction()
+
 function(uberton_add_cmrc_resource_folder)
     set(args NAME FOLDER WHENCE)
     cmake_parse_arguments(PARSE_ARGV 0 ARG "${options}" "${args}" "${list_args}")
@@ -42,8 +57,8 @@ function(uberton_add_cmrc_resource_folder)
 
     cmrc_add_resource_library(rc_${ARG_NAME} ALIAS ${ARG_NAME} NAMESPACE ${ARG_NAME} ${file_list})
     target_link_libraries(${target} PUBLIC ${ARG_NAME})
+	set_target_properties(rc_${ARG_NAME} PROPERTIES ${UBERTON_INSTALLER_RESOURCE_FOLDER})
 endfunction()
-
 
 
 function (uberton_add_plugin_installer)
@@ -58,7 +73,13 @@ function (uberton_add_plugin_installer)
 		"${ABSOLUTE_INSTALLER_PATH}/source/main.cpp"
 	)
 	vstgui_add_executable(${target} "${${target}_sources}")
-	
+	if(SMTG_WIN)
+		set_target_properties(${target} PROPERTIES LINK_FLAGS "/INCLUDE:wWinMain /MANIFESTUAC:\"level='requireAdministrator' uiAccess='false'\" ")
+		#target_link_options(${target} PUBLIC LINK_OPTIONS "MANIFESTUAC:\"level='requireAdministrator' uiAccess='false'\" ")
+		#add_link_options("/MANIFESTUAC:\"level='requireAdministrator' uiAccess='false'\" ")
+	endif()
+
+
     get_target_property(PLUGIN_BINARY_DIR   ${ARG_NAME} SMTG_PLUGIN_BINARY_DIR)
     get_target_property(PLUGIN_PACKAGE_NAME ${ARG_NAME} SMTG_PLUGIN_PACKAGE_NAME)
     get_target_property(TARGET_SOURCE       ${ARG_NAME} SMTG_PLUGIN_PACKAGE_PATH)
@@ -73,15 +94,17 @@ function (uberton_add_plugin_installer)
 	if(UBERTON_PLUGIN_HAS_USERGUIDE_PDF)
 		get_target_property(UBERTON_PLUGIN_USERGUIDE_PDF ${ARG_NAME} UBERTON_USERGUIDE_PDF)
 		set(userguide_pdf "userguide_pdf")
-		cmrc_add_resource_library(rc_${userguide_pdf} ALIAS ${userguide_pdf} NAMESPACE ${userguide_pdf} ${UBERTON_PLUGIN_USERGUIDE_PDF})
-		target_link_libraries(${target} PUBLIC ${userguide_pdf})
+		uberton_add_cmrc_resource(NAME "${ARG_NAME}_${userguide_pdf}" FILE ${UBERTON_PLUGIN_USERGUIDE_PDF})
+		#cmrc_add_resource_library(rc_${userguide_pdf} ALIAS ${userguide_pdf} NAMESPACE ${userguide_pdf} ${UBERTON_PLUGIN_USERGUIDE_PDF})
+		#set_target_properties(rc_${userguide_pdf} PROPERTIES "Installer/Resource_Projects")
+		#target_link_libraries(${target} PUBLIC ${userguide_pdf})
 	endif()
 
 	get_property(UBERTON_PLUGIN_HAS_FACTORY_PRESETS TARGET ${ARG_NAME} PROPERTY UBERTON_FACTORY_PRESETS SET)
 	if(UBERTON_PLUGIN_HAS_FACTORY_PRESETS)
 		get_target_property(UBERTON_PLUGIN_FACTORY_PRESETS_PATH ${ARG_NAME} UBERTON_FACTORY_PRESETS)
 		set(factory_presets "factory_presets")
-		uberton_add_cmrc_resource_folder(NAME ${factory_presets} FOLDER ${UBERTON_PLUGIN_FACTORY_PRESETS_PATH})
+		uberton_add_cmrc_resource_folder(NAME "${ARG_NAME}_${factory_presets}" FOLDER ${UBERTON_PLUGIN_FACTORY_PRESETS_PATH})
 	endif()
 	
 	configure_file("${ABSOLUTE_INSTALLER_PATH}/source/plugin_defs.h.in" plugin_defs.h)
@@ -94,4 +117,5 @@ function (uberton_add_plugin_installer)
 	target_include_directories(${target} PUBLIC ${PROJECT_BINARY_DIR})
 	target_include_directories(${target} PUBLIC "${UBERTON_SRC_PATH}/src/installer/source")
 	set_target_properties(${target} PROPERTIES ${UBERTON_INSTALLER_FOLDER})
+
 endfunction()
