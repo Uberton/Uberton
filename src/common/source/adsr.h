01f4f49a5446d6368c6e5014eb0846b616db41ee
Mc-zen <alphabetagamma11@hotmail.com>
2022-03-06 22:06:31 +0100

    [add] Add adsr envelope with power curves (up to power 4).
    - use long long for adsr time index to prevent overflow that could happen with note holds of more than a few hours

--- src/common/source/adsr.h
+++ src/common/source/adsr.h
@@ -11,7 +11,7 @@
 // have received a copy of the GNU General Public License along with Überton. If not, see http://www.gnu.org/licenses/.
 // -----------------------------------------------------------------------------------------------------------------------------
 
-
+#include <cstdint>
 
 #pragma once
 
@@ -38,9 +38,11 @@ namespace Uberton {
 //
 class QuadraticADSREnvelope
 {
+	using Int = long long;
+
 public:
 	// setParams should better not be called betwee start() and release()
-	void setParams(int attackSamples, int decaySamples, double sustainLevel, int releaseSamples) noexcept {
+	void setParams(Int attackSamples, Int decaySamples, double sustainLevel, Int releaseSamples) noexcept {
 		ca = 2 * 1.0 / (attackSamples * (attackSamples - 1.0));
 		cd = -2 * (1.0 - sustainLevel) / (decaySamples * (decaySamples - 1.0));
 		attackEnd = attackSamples;
@@ -92,19 +94,19 @@ public:
 
 
 private:
-	int attackEnd{ 1 };
-	int decayEnd{ 1 };
-	int releaseEnd{ 1 };
-	int releaseTime{ 1 };
+	Int attackEnd{ 1 };
+	Int decayEnd{ 1 };
+	Int releaseEnd{ 1 };
+	Int releaseTime{ 1 };
 
-	int a0{};
+	Int a0{};
 	double c0{};
 
 	bool constant{ false }; // true during sustain and after release
-	int nextBreak{};		// index of next period switch
+	Int nextBreak{};		// index of next period switch
 
 	double ca{}, cd{}; // time coefficients for attack and release period
-	int index{ 0 };	   // sample count
+	Int index{ 0 };	   // sample count
 	double value{ 0 }; // current velocity
 };
 
@@ -125,8 +127,14 @@ private:
 //
 class LinearADSREnvelope
 {
+	// using a sample rate of 192000
+	// we can use this for up to
+	//  - (2^31-1)/192000 ≈ 3 hours with 32 bit int
+	//  -  (2^63-1)/192000 ≈ 13344 million hours with 32 bit int
+	using Int = long long;
+
 public:
-	void set(int attackSamples, int decaySamples, double sustainLevel, int releaseSamples) noexcept {
+	void set(Int attackSamples, Int decaySamples, double sustainLevel, Int releaseSamples) noexcept {
 		a = attackSamples;
 		d = decaySamples;
 		s = sustainLevel;
@@ -134,15 +142,15 @@ public:
 
 		if (index < attackEnd) { // if still in attack phase, update attack ramp, otherwise ignore new attack value
 			currentRamp = 1. / a;
-			attackEnd = static_cast<int>((1. - value) * a) + index;
+			attackEnd = static_cast<Int>((1. - value) * a) + index;
 			nextBreak = attackEnd;
 		} else if (index < decayEnd) { // if still in decay phase, update decay ramp, otherwise ignore new attack value
 			currentRamp = (s - 1.) / d;
-			decayEnd = static_cast<int>(-(value - s) / currentRamp) + index;
+			decayEnd = static_cast<Int>(-(value - s) / currentRamp) + index;
 			nextBreak = decayEnd;
 		} else if (index < releaseEnd) {
 			currentRamp = -releaseValue / r;
-			releaseEnd = static_cast<int>(-value / currentRamp) + index;
+			releaseEnd = static_cast<Int>(-value / currentRamp) + index;
 			nextBreak = releaseEnd;
 		}
 	}
@@ -154,6 +162,7 @@ public:
 		currentRamp = 1. / a;
 		attackEnd = a;
 		nextBreak = attackEnd;
+		releaseEnd = -1;
 	}
 
 	void release() noexcept {
@@ -167,18 +176,19 @@ public:
 	}
 
 	double next() noexcept {
-		if (constant) return value;
-		value += currentRamp;
-		if (++index == nextBreak) {
-			if (nextBreak == attackEnd) {
-				currentRamp = (s - 1.) / d;
-				decayEnd = attackEnd + d;
-				nextBreak = decayEnd;
-			} else if (nextBreak == decayEnd) {
-				constant = true;
-			} else if (nextBreak == releaseEnd) {
-				value = 0;
-				constant = true;
+		if (!constant) {
+			value += currentRamp;
+			if (++index == nextBreak) {
+				if (nextBreak == attackEnd) {
+					currentRamp = (s - 1.) / d;
+					decayEnd = attackEnd + d;
+					nextBreak = decayEnd;
+				} else if (nextBreak == decayEnd) {
+					constant = true;
+				} else if (nextBreak == releaseEnd) {
+					value = 0;
+					constant = true;
+				}
 			}
 		}
 		return value;
@@ -192,21 +202,159 @@ public:
 		return index == releaseEnd;
 	}
 
+	bool isInRelease() const noexcept {
+		return nextBreak == releaseEnd;
+	}
+
 
 private:
 	double currentRamp{};
 
-	int a{}, d{}, r{};
+	Int a{}, d{}, r{};
 	double s{};
 
-	int attackEnd{ 1 };
-	int decayEnd{ 1 };
-	int releaseEnd{ 1 };
+	Int attackEnd{};
+	Int decayEnd{};
+	Int releaseEnd{ -1 };
 
 	bool constant{ false }; // true during sustain and after release
 	int nextBreak{};		// index of next period switch
 
-	int index{};	// sample count
+	Int index{};	// sample count
+	double value{}; // current velocity
+
+	double releaseValue{}; // value at time of release
+};
+
+
+// 
+// Same specification as LinearADSREnvelope but with power functions as curves which can be 
+// linear (1), quadratic (2), cubic (3) or quartic (4). 
+// 
+// The parameter can be chosen to resemble an exponential curve. This way, when applied to a level,
+// the change in perceived loudness is approximately linear while for a linear envelope it would be 
+// logarithmic. All curves are concave. 
+// 
+//     .
+//     |
+//    | \________
+//   /           |
+//  /             \_____
+//
+
+template<int n>
+class NaturalPowerADSREnvelope
+{
+	using Int = long long;
+
+public:
+	static_assert(n >= 1 && n <= 4, "Template parameter n can only take values 1,2,3,4");
+
+
+	void set(Int attackSamples, Int decaySamples, double sustainLevel, Int releaseSamples) noexcept {
+		a = attackSamples;
+		d = decaySamples;
+		if constexpr (n == 1)
+			s = sustainLevel;
+		else if constexpr (n == 2)
+			s = std::sqrt(sustainLevel);
+		else if constexpr (n == 3)
+			s = std::pow(sustainLevel, 1.0 / 3.0);
+		else if constexpr (n == 4)
+			s = std::pow(sustainLevel, 1.0 / 4.0);
+		r = releaseSamples;
+
+		if (index < attackEnd) { // if still in attack phase, update attack ramp, otherwise ignore new attack value
+			currentRamp = 1. / a;
+			attackEnd = static_cast<Int>((1. - value) * a) + index;
+			nextBreak = attackEnd;
+		} else if (index < decayEnd) { // if still in decay phase, update decay ramp, otherwise ignore new attack value
+			currentRamp = (s - 1.) / d;
+			decayEnd = static_cast<Int>(-(value - s) / currentRamp) + index;
+			nextBreak = decayEnd;
+		} else if (index < releaseEnd) {
+			currentRamp = -releaseValue / r;
+			releaseEnd = static_cast<Int>(-value / currentRamp) + index;
+			nextBreak = releaseEnd;
+		}
+	}
+
+	void reset() noexcept {
+		index = 0;
+		value = 0;
+		constant = false;
+		currentRamp = 1. / a;
+		attackEnd = a;
+		nextBreak = attackEnd;
+		releaseEnd = -1;
+	}
+
+	void release() noexcept {
+		releaseValue = value;
+		currentRamp = -value / r;
+		releaseEnd = index + r;
+		nextBreak = releaseEnd;
+		attackEnd = 0; // important in case release() is called during attack phase
+		decayEnd = 0;  // important in case release() is called during decay phase
+		constant = false;
+	}
+
+	double next() noexcept {
+		if (!constant) {
+			value += currentRamp;
+			if (++index == nextBreak) {
+				if (nextBreak == attackEnd) {
+					currentRamp = (s - 1.) / d;
+					decayEnd = attackEnd + d;
+					nextBreak = decayEnd;
+				} else if (nextBreak == decayEnd) {
+					constant = true;
+				} else if (nextBreak == releaseEnd) {
+					value = 0;
+					constant = true;
+				}
+			}
+		}
+		if constexpr (n == 1) {
+			return value;
+		} else if constexpr (n == 2) {
+			return value * value;
+		} else if constexpr (n == 3) {
+			auto sq = value * value;
+			return value * sq;
+		} else if constexpr (n == 4) {
+			auto sq = value * value;
+			return sq * sq;
+		}
+	}
+
+	bool hasReachedSustain() const noexcept {
+		return index == decayEnd;
+	}
+
+	bool isFinished() const noexcept {
+		return index == releaseEnd;
+	}
+
+	bool isInRelease() const noexcept {
+		return nextBreak == releaseEnd;
+	}
+
+
+private:
+	double currentRamp{};
+
+	Int a{}, d{}, r{};
+	double s{};
+
+	Int attackEnd{};
+	Int decayEnd{};
+	Int releaseEnd{ -1 };
+
+	bool constant{ false }; // true during sustain and after release
+	int nextBreak{};		// index of next period switch
+
+	Int index{};	// sample count
 	double value{}; // current velocity
 
 	double releaseValue{}; // value at time of release
